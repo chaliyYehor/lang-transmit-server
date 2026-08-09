23be63e4335531ffdecf0b74a52c252e805e2f78
Yehor <chaliyegor00@gmail.com>
2026-08-09 19:53:47 +0300

    add a validation schema for the data of incoming message

---
+++ src/schemas/joinRoomSchema.ts
@@ -0,0 +1,8 @@
+import z from 'zod'
+
+export const joinRoomSchema = z.object({
+	type: z.enum(['pc', 'user'], 'Invalid message type'),
+	roomNum: z.string().min(3, 'Room Id Is Too Short')
+})
+
+export type JoinRoom = z.infer<typeof joinRoomSchema>
